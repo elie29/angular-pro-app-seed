8a170640f88caad6a78f8ac617cf27b5d50a5754
Elie NEHME <elie29@gmail.com>
2018-01-26 14:29:40 +0100

    USe change detection

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,25 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 import { Store } from './store';
 
 @Component({
   selector: 'app-root',
   template: `
-    <div>
-      <div *ngFor="let todo of todos$ | async">
-        {{ todo.name }}
-      </div>
+    <div *ngFor="let todo of todos$ | async">
+      {{ todo.name }}
     </div>
-  `
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-
   todos$ = this.store.select<any[]>('todos');
 
-  constructor(
-    private store: Store
-  ) {
-    this.store.set('todos', [{ id: 1, name: 'Eat dinner' }, { id: 2, name: 'Do washing' }]);
+  constructor(private store: Store) {
+    this.store.set('todos', [
+      { id: 1, name: 'Eat dinner' },
+      { id: 2, name: 'Do washing' }
+    ]);
   }
-
 }
